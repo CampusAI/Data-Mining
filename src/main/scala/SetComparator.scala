71a65cc1294e11efe692254e56e59e74a4b761d9
OleguerCanal <oleguer.canal@hotmail.com>
2020-10-31 19:19:25 +0100

    Removed debug line

--- src/main/scala/SetComparator.scala
+++ src/main/scala/SetComparator.scala
@@ -1,7 +1,6 @@
 class SetComparator {
   def getJaccardSim(s1 : scala.collection.SortedSet[Int], s2 : scala.collection.SortedSet[Int]) : Float = {
     var intersection = s1.intersect(s2)
-    intersection.foreach(element => println(element))
     var union = s1.union(s2)
     return intersection.size.toFloat/union.size.toFloat
   }
