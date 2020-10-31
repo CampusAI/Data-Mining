a3cbb941da411c96a4ac19c4398b51ea208fb507
OleguerCanal <oleguer.canal@hotmail.com>
2020-10-31 19:31:48 +0100

    Added unordered set option

--- src/main/scala/Shingler.scala
+++ src/main/scala/Shingler.scala
@@ -7,13 +7,17 @@ class Shingler(shingle_len: Int, hash_bins : Int) {
     return math.abs(str.hashCode % hash_bins)
   }
   
-  def getHashedShingles(doc : String) : scala.collection.SortedSet[Int] = {
+  def getOrderedHashShingles(doc : String) : scala.collection.SortedSet[Int] = {
     return collection.immutable.SortedSet[Int]() ++ getShingles(doc).map(getHash)
+  }
+  
+  def getHashShingles(doc : String) : Set[Int] = {
+    return getShingles(doc).map(getHash)
   }
 }
 
 // // Usage example:
 // var doc = "abcde"
 // var shingler = new Shingler(2, 3)
-// var shingles = shingler.getHashedShingles(doc1)
+// var shingles = shingler.getOrderedHashShingles(doc1)
 // shingles.foreach(element => println(element))
