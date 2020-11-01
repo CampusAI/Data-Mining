ebf7819b5e942236f1aad9cea00d130a362acf59
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-01 22:01:04 +0100

    Added docstring

--- src/main/scala/MinHasher.scala
+++ src/main/scala/MinHasher.scala
@@ -1,3 +1,5 @@
+package hashing
+
 import scala.util.Random
 
 /** Represents an element of a Family of Hash function
@@ -10,29 +12,39 @@ import scala.util.Random
 class Hasher(seed: Int, max_val : Int, p : Int = 104729) {
   // https://stackoverflow.com/questions/19701052/how-many-hash-functions-are-required-in-a-minhash-algorithm
   private val random_generator = new scala.util.Random(seed)
-  val a = 1 + 2*random_generator.nextInt((p-2)/2)// a odd in [1, p-1]
+  val a = 1 + 2*random_generator.nextInt((p-2)/2) // a odd in [1, p-1]
   val b = 1 + random_generator.nextInt(p - 2) // b in [1, p-1]
   
   /** Get the hash value of the inputed integer
-   *  @param x Integer from which to get the hash value
+   *  @param x the integer from which to get the hash value
+   *  @return the hash value
    */
   def getHash(x : Int) : Int = {
     return ((a*x + b) % p) % max_val
   }
 }
 
-
+/** Implements set minhashing functionality
+ *
+ *  @constructor store the hashes
+ *  @param hashes the list of hash functions to use (random permutations)
+ */
 class MinHasher(hashes : List[Hasher]) {
-  def getMinHash(hasher : Hasher)(set : Set[Int]) : Int = {
-    return set.map(hasher.getHash).min
+
+  /** Get the MinHash of a set applying the provided hash function (permutation)
+    *  @param hasher the hash function used to permute the set ids
+    *  @param set the ids from which to find the MinHash
+    *  @return Minhash of the set once the permutation has been applied
+    */
+  def getMinHash(hash : Hasher)(set : Set[Int]) : Int = {
+    return set.map(hash.getHash).min
   }
   
+  /** Get the MinHash List of the given set applying the stored hash functions (permutation)
+    *  @param set the ids from which to find the MinHash List
+    *  @return List of minhashes
+    */
   def getMinHashes(set: Set[Int]) : List[Int] = {
     return hashes.map(getMinHash(set))
   }
 }
-
-// How to create list of Hash functions
-// val max_hash_val = 10
-// val minhash_len = 7
-// val x = List.tabulate(minhash_len)(seed => new Hasher(seed, max_hash_val))
