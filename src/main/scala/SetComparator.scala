2b70b2ea9518a679ce199bec4684dbbc35c8c31d
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-01 17:34:18 +0100

    Var to val

--- src/main/scala/SetComparator.scala
+++ src/main/scala/SetComparator.scala
@@ -1,13 +1,13 @@
 class SetComparator {
   def getJaccardSim(s1 : scala.collection.SortedSet[Int], s2 : scala.collection.SortedSet[Int]) : Float = {
-    var intersection = s1.intersect(s2)
-    var union = s1.union(s2)
+    val intersection = s1.intersect(s2)
+    val union = s1.union(s2)
     return intersection.size.toFloat/union.size.toFloat
   }
 
    def getJaccardSim(s1 : Set[Int], s2 : Set[Int]) : Float = {
-    var intersection = s1.intersect(s2)
-    var union = s1.union(s2)
+    val intersection = s1.intersect(s2)
+    val union = s1.union(s2)
     return intersection.size.toFloat/union.size.toFloat
   }
 }
