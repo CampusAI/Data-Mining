c23b15e3f56de7501cf505bb00ae662aafa1faab
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-01 20:57:32 +0100

    Added MinHasher

--- src/main/scala/MinHasher.scala
+++ src/main/scala/MinHasher.scala
@@ -7,7 +7,7 @@ import scala.util.Random
  *  @param seed the random hash parameters seed
  *  @param p prime number which should be larger than the largest inputed value
  */
-class Hasher(max_val : Int, seed: Int, p : Int = 104729) {
+class Hasher(seed: Int, max_val : Int, p : Int = 104729) {
   // https://stackoverflow.com/questions/19701052/how-many-hash-functions-are-required-in-a-minhash-algorithm
   private val random_generator = new scala.util.Random(seed)
   val a = 1 + 2*random_generator.nextInt((p-2)/2)// a odd in [1, p-1]
@@ -22,7 +22,17 @@ class Hasher(max_val : Int, seed: Int, p : Int = 104729) {
 }
 
 
+class MinHasher(hashes : List[Hasher]) {
+  def getMinHash(hasher : Hasher)(set : Set[Int]) : Int = {
+    return set.map(hasher.getHash).min
+  }
+  
+  def getMinHashes(set: Set[Int]) : List[Int] = {
+    return hashes.map(getMinHash(set))
+  }
+}
+
 // How to create list of Hash functions
 // val max_hash_val = 10
 // val minhash_len = 7
-// val x = List.tabulate(minhash_len)(n => new Hasher(n)(max_hash_val))
+// val x = List.tabulate(minhash_len)(seed => new Hasher(seed, max_hash_val))
