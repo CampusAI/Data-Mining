061c739e4a56f0c146e8a1a20ee11cb5f15beb20
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-04 15:53:58 +0100

    Automatically assinging doc id

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,18 +18,18 @@ object Main {
     spark.sparkContext.setLogLevel("ERROR")
 
     // Load data
-    val docs = Seq((0, "qwertyuiopasdfghjkl"),
-                   (1, "qwertyvkfjdfknfdjdfasdfg"))
-    var df = docs.toDF("id", "text") // var or val?
+    val docs = Seq("qwertyuiopasdfghjkl", "qwertyvkfjdfknfdjdfasdfg")
+    var df = docs.toDF("text") // var or val?
 
     // Shingler
     val shingle_len = 3
     val shingle_bins = 100
     var shingler = new Shingler(shingle_len, shingle_bins)
 
-
     val hashShinglesUDF = udf[Seq[Int], String](shingler.getHashShingles)
     df = df.withColumn("hashed_shingles", hashShinglesUDF('text))
+
+    df = df.withColumn("id",monotonically_increasing_id()
     df = df.crossJoin(df.withColumnRenamed("hashed_shingles", "hashed_shingles2").withColumnRenamed("id", "id_j"))
 
     val jaccardSimUDF = udf((set1: Seq[Int], set2: Seq[Int]) => (set1.intersect(set2).toSet.size.toFloat )/ (set1 ++ set2).toSet.size.toFloat)
