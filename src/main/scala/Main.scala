16360e8e5eec6538eaab44d86a4d3381df08316f
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-06 22:16:30 +0100

    Refactored main to get statistics of each approach

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,10 @@ import System.{exit, nanoTime}
 
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
-import org.apache.spark.sql.{Column, SparkSession}
+import org.apache.spark.sql.{Column, SparkSession, DataFrame}
 import org.apache.spark.sql.functions.{array_repeat, arrays_zip, col, count, desc, exists, expr, hash, length, monotonically_increasing_id, transform, udf}
+import spark.implicits._
+
 import hashing.{Hasher, MinHasher, LSH}
 
 object Main {
@@ -17,56 +19,120 @@ object Main {
       result
   }
 
-  def main() {
-    val spark = SparkSession.builder.appName("SimpleApplication")
-      .config("spark.master", "local[4]")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    import spark.implicits._
-    val docs = Seq(
-      // "/home/fedetask/Downloads/text_dataset/Part1/awards_1990/awd_1990_00/*"
-      "/home/oleguer/Documents/p6/Data-Mining/datasets/Part1/awards_1990/awd_1990_00/*"
-    )
-    var df = spark.read.option("wholetext", true)
-      .text(docs: _*).toDF("text")
+  def LoadData(spark: SparkSession, shingle_len: Int=5, path: String): DataFrame = {
+    // Read data
+    val docs = Seq(path)
+    var df = spark.read.option("wholetext", true).text(docs: _*).toDF("text")
     df = df.withColumn("id", monotonically_increasing_id())
 
+    // Compute Hashed Shingles
+    df = df.withColumn("tmp", array_repeat($"text", length($"text") - shingle_len + 1))
+    val shingles_expr = "transform(tmp,(x,i) -> substring(x from i+1 for " + shingle_len + "))"
+    df = df.withColumn("shingles", expr(shingles_expr))
+    df = df.withColumn("hashed_shingles", transform($"shingles", (x) => hash(x)))
+    df = df.withColumn("id", monotonically_increasing_id())
+    return df
+  }
+
+  def real() {
+    val shingle_len = 5
+    val similarity_threshold = 0.8
+    val path = "/home/oleguer/Documents/p6/Data-Mining/datasets/Part1/awards_1990/awd_1990_00/*"
+
+    // Start spark
+    val spark = SparkSession.builder.appName("SimpleApplication").config("spark.master", "local[4]").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    var df = LoadData(spark, shingle_len, path)
+
     time {
-      // Shingler
-      val shingle_len = 5
+      // Cross all combinations
+      df = df.crossJoin(
+          df.withColumnRenamed("hashed_shingles", "hashed_shingles2")
+            .withColumnRenamed("id", "id_j")
+            .withColumnRenamed("text", "text2")).filter($"id" < $"id_j")
+
+        // Real distance
+        val jaccardSimUDF = udf(
+          (s1: Seq[Int], s2: Seq[Int]) => (s1.intersect(s2).toSet.size.toFloat) / (s1 ++ s2).toSet.size.toFloat
+        )
+        df = df.withColumn("jaccardSim", jaccardSimUDF($"hashed_shingles", $"hashed_shingles2"))
+        df = df.filter($"jaccardSim" > similarity_threshold)
+        println("Real similar documents: " + df.count())
+    }
+    spark.stop()
+  }
+
+  def minhashing() {
+    val minhash_len = 100
+    val shingle_len = 5
+    val similarity_threshold = 0.8
+    val path = "/home/oleguer/Documents/p6/Data-Mining/datasets/Part1/awards_1990/awd_1990_00/*"
+
+    // Start spark
+    val spark = SparkSession.builder.appName("SimpleApplication").config("spark.master", "local[4]").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    var df = LoadData(spark, shingle_len, path)
+
+    time {
       // Minhasher
-      val minhash_len = 100
       val hashes = List.tabulate(minhash_len)(n => new Hasher(n))
       val minhasher = new MinHasher(hashes)
-      // LSH
-      val bands = 10
-      val lshasher = new LSH(bands)
-      val similarity_threshold = 0.8
+      val minhasherUDF = udf[Seq[Int], Seq[Int]](minhasher.getMinHashes)
+      df = df.withColumn("minhashes", minhasherUDF('hashed_shingles))
+    
+      // Cross all combinations
+      df = df.crossJoin(
+          df.withColumnRenamed("hashed_shingles", "hashed_shingles2")
+            .withColumnRenamed("id", "id_j")
+            .withColumnRenamed("minhashes", "minhashes2")
+            .withColumnRenamed("text", "text2")).filter($"id" < $"id_j")
 
-      // Compute Hashed Shingles
-      df = df.withColumn("tmp", array_repeat($"text", length($"text") - shingle_len + 1))
-      val shingles_expr = "transform(tmp,(x,i) -> substring(x from i+1 for " + shingle_len + "))"
-      df = df.withColumn("shingles", expr(shingles_expr))
-      df = df.withColumn("hashed_shingles", transform($"shingles", (x) => hash(x)))
+        // Aprox distance
+        val compareSignaturesUDF = udf(
+          (s1: Seq[Int], s2: Seq[Int]) => s1.zip(s2).count({ case (x, y) => x == y }).toFloat / s1.size.toFloat
+        )
+        df = df.withColumn("approxJaccardSim", compareSignaturesUDF($"minhashes", $"minhashes2"))
 
-      df = df.withColumn("id", monotonically_increasing_id())
+        df = df.filter($"approxJaccardSim" > similarity_threshold)
+        println("Matched by approximate Jac similarity: " + df.count())
+    }
+    spark.stop()
+  }
 
-      // Compute Minhashes
+  def lhs() {
+    val minhash_len = 100
+    val shingle_len = 5
+    val bands = 10
+    val similarity_threshold = 0.8
+    val path = "/home/oleguer/Documents/p6/Data-Mining/datasets/Part1/awards_1990/awd_1990_00/*"
+
+    // Start spark
+    val spark = SparkSession.builder.appName("SimpleApplication").config("spark.master", "local[4]").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    var df = LoadData(spark, shingle_len, path)
+
+    time {
+      // Minhasher
+      val hashes = List.tabulate(minhash_len)(n => new Hasher(n))
+      val minhasher = new MinHasher(hashes)
       val minhasherUDF = udf[Seq[Int], Seq[Int]](minhasher.getMinHashes)
       df = df.withColumn("minhashes", minhasherUDF('hashed_shingles))
 
       // Compute LSH
+      val lshasher = new LSH(bands)
       val lhasherUDF = udf[Seq[Int], Seq[Int]](lshasher.hashBands)
       df = df.withColumn("ls_hashes", lhasherUDF($"minhashes"))
-
+    
       // Cross all combinations
       df = df.crossJoin(
-        df.withColumnRenamed("hashed_shingles", "hashed_shingles2")
-          .withColumnRenamed("id", "id_j")
-          .withColumnRenamed("minhashes", "minhashes2")
-          .withColumnRenamed("ls_hashes", "ls_hashes2")
-          .withColumnRenamed("text", "text2")
-      ).filter($"id" < $"id_j")
+          df.withColumnRenamed("hashed_shingles", "hashed_shingles2")
+            .withColumnRenamed("id", "id_j")
+            .withColumnRenamed("minhashes", "minhashes2")
+            .withColumnRenamed("ls_hashes", "ls_hashes2")
+            .withColumnRenamed("text", "text2")).filter($"id" < $"id_j")
 
       // LSH matching
       df = df.withColumn("lsh_match", exists(
@@ -74,22 +140,15 @@ object Main {
       )).filter($"lsh_match" === true)
       println("Matched by LSH: " + df.count())
 
-      // Aprox distance
+      // Approx distance
       val compareSignaturesUDF = udf(
         (s1: Seq[Int], s2: Seq[Int]) => s1.zip(s2).count({ case (x, y) => x == y }).toFloat / s1.size.toFloat
       )
       df = df.withColumn("approxJaccardSim", compareSignaturesUDF($"minhashes", $"minhashes2"))
+
       // Clean by false positives
       df = df.filter($"approxJaccardSim" > similarity_threshold)
       println("Matched by approximate Jac similarity: " + df.count())
-
-      // Real distance
-      val jaccardSimUDF = udf(
-        (s1: Seq[Int], s2: Seq[Int]) => (s1.intersect(s2).toSet.size.toFloat) / (s1 ++ s2).toSet.size.toFloat
-      )
-      df = df.withColumn("jaccardSim", jaccardSimUDF($"hashed_shingles", $"hashed_shingles2"))
-      println("True positives: " + df.count())
-
     }
     spark.stop()
   }
