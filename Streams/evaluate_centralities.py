902023cd5373a3f06c9360365fd12cbdcf396a25
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-22 13:04:24 +0100

    Computing our centralities error

--- Streams/evaluate_centralities.py
+++ Streams/evaluate_centralities.py
@@ -2,6 +2,9 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 
+from centrality import compute_centralities
+
+
 EPS_ = 1e-5
 
 def get_ground_truth(graph_file):
@@ -27,15 +30,28 @@ def get_MAPE(approx, ground_truth):
     for key in approx:
         error += abs((approx[key] - ground_truth[key])/(ground_truth[key] + EPS_))
         n += 1.
-    return 100*error/n
+    return 100.*error/n
 
 if __name__=="__main__":
     # graph_file = "datasets/graph.csv"
     # graph_file = "datasets/citations.csv"
     graph_file = "datasets/emails.csv"
+    
+    # Our centrality
+    print("Computing approximate centrality")
+    approx_centralities = compute_centralities(graph_file='datasets/emails.csv', sphere_dir='datasets/emails/')
+    print("DONE")
+    
+    # Ground truth
     print("Computing ground truth...")
     closeness_cent_gt, harmonic_cent_gt = get_ground_truth(graph_file)
     print("DONE")
 
-    error = get_MAPE(closeness_cent_gt, harmonic_cent_gt)
-    print("error:", error, "%")
+    # Compute errors
+    closeness_error = get_MAPE(approx=approx_centralities["closeness_centrality"],
+                               ground_truth=closeness_cent_gt)
+    print("closeness_error:", closeness_error, "%")
+
+    harmonic_error = get_MAPE(approx=approx_centralities["harmonic_centralities"],
+                               ground_truth=harmonic_cent_gt)
+    print("harmonic_error:", harmonic_error, "%")
