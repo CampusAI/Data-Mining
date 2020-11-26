426cbc3f608303cb2a30bc34f2600deb3dd9569d
OleguerCanal <oleguer.canal@hotmail.com>
2020-11-26 17:07:54 +0100

    Testing counter

--- Streams/counter.py
+++ Streams/counter.py
@@ -3,16 +3,16 @@ import pathlib
 
 from hashlib import md5
 import numpy as np
+from tqdm import tqdm
 import random
 from scipy.integrate import quad
 from struct import unpack
-
 
 class Counter:
 
     DEFAULT_REGISTERS = 5
 
-    def __init__(self, b):
+    def __init__(self, b=5):
         self.b = b
         self.p = 2**b
         self.M = np.zeros(self.p)
@@ -23,13 +23,12 @@ class Counter:
             return (np.log2((2 + u)/(1 + u)))**self.p
         self.alpha_p = (self.p*quad(integrand, 0, np.inf)[0])**-1
 
-    def add(self, hashed_elem):
+    def add(self, elem):
         bin_representation = "{0:b}".format(hashed_elem).zfill(self.bin_repr_len)
-        i = int(bin_representation[:self.b], 2)  # Position
+        bucket = int(bin_representation[:self.b], 2)  # Bucket
         p_plus = bin_representation[self.b:].find("1") + 1  # Leading zeros
-        p_plus = self.bin_repr_len - self.b + 1 if p_plus == 0 else p_plus
-        self.M[i] = max(self.M[i], p_plus)
-
+        p_plus = self.bin_repr_len - self.b + 1 if p_plus == 0 else p_plus # Deal with all 0's case
+        self.M[bucket] = max(self.M[bucket], p_plus)
     def hash_add(self, elem):
         # Hash value
         hashed_elem = unpack("<IIII",md5(elem).digest())[0]
@@ -69,21 +68,29 @@ class Counter:
         """
         return np.array_equal(self.M, count.M)
 
-
 if __name__ == "__main__":
-    b = 5
-    counter = Counter(b=b)
+    errors = []
 
-    num_dif_values = 10
-    elems = np.array(list(set(
-        [random.randint(0, 2**32-1) for _ in range(num_dif_values)])
-    ), dtype=np.int32)
-    num_dif_values = len(elems)
+    for _ in tqdm(range(100)):
+        counter = Counter(b=5)
+        num_dif_values = random.randint(10, 1000)
+        elems = np.array(list(set(
+            [random.randint(0, 2**32-1) for _ in range(num_dif_values)])
+        ), dtype=np.int32)
+        num_dif_values = len(elems)
 
-    iterations = 15
-    for _ in range(iterations):
-        elem = np.random.choice(elems)
-        counter.hash_add(elem=elem)
+        iterations = random.randint(10, 10000)
+        for _ in range(iterations):
+            elem = np.random.choice(elems)
+            counter.add(elem=elem)
 
-    print("Real diff values: ", num_dif_values)
-    print("Approx diff values: ", counter.size())
+        rel_error = (num_dif_values - counter.size())/num_dif_values
+        errors.append(rel_error)
+
+    error_mean = np.average(errors)
+    error_std = np.std(errors)
+
+    print(errors)
+
+    print("error_mean", error_mean)
+    print("error_std", error_std)
